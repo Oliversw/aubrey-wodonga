b5c6f5b0b9c24b4e57c6a0651e1e81c33ab41438
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2020-11-09 23:36:22 +0000

    Add SVG package for Gatsby and move assets folder accordingly

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,5 +51,13 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/, // See below to configure properly
+        },
+      },
+    },
   ],
 };

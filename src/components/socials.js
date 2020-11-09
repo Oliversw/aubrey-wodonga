8ac14e65884f666ef46759325a77931acd30f11f
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2020-11-09 23:57:57 +0000

    Space out social icons

--- src/components/socials.js
+++ src/components/socials.js
@@ -1,18 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
 import Instagram from "../../assets/socialInsta.svg";
 import Tiktok from "../../assets/socialTiktok.svg";
 import Twitch from "../../assets/socialTwitch.svg";
 import Twitter from "../../assets/socialTwitter.svg";
 
+const Container = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+  align-items: center;
+`;
+
 const Socials = () => {
   return (
-    <>
+    <Container>
       <Instagram />
       <Twitch />
       <Twitter />
       <Tiktok />
-    </>
+    </Container>
   );
 };
 

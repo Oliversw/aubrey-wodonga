6dcc68d43355d6433853369b4dc1b642323578b4
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2020-11-19 18:48:24 +0000

    Reintroduce css import for now

--- src/components/globalStyle.js
+++ src/components/globalStyle.js
@@ -55,6 +55,8 @@ table {
 
 /* -------------------- BEGIN GLOBAL STYLES -------------------- */
 
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,600;0,800;1,400&display=swap');
+
 
 :root {
   --black: #121212;
